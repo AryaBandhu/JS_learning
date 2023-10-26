3d89c8dcaf5c7f05a2f1c4d717cca76a2eda7977
Arya Bandhu <73266677+AryaBandhu@users.noreply.github.com>
2023-10-26 18:43:38 +0000

    variable Understand Done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+const aryaRank = 22142
+let aryaEmail = "arya@gmail.com"
+var aryaPassword = "12345"
+// again we can't use var
+aryaCity = "Dehradun"
+
+let aryaNewCity = "Bengaluru"
+let aryaMobileNumber;
+
+
+// aryaRank = 42535
+// we can't change const variable
+
+console.log(aryaRank);
+/*
+this is for multiline comment 
+*/
+console.table([aryaEmail, aryaPassword, aryaCity, aryaNewCity, aryaMobileNumber])
